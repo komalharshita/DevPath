02607d2c59ff1b76f5a08b4d59676d8289d5b643
Yash Vijay <ktanayash@gmail.com>
2026-06-27 16:36:37 +0530

    fix: remove unnecessary blob: from script-src CSP directive

--- src/app.py
+++ src/app.py
@@ -46,7 +46,7 @@ def add_security_headers(response):
     )
     response.headers["Content-Security-Policy"] = (
         "default-src 'self'; "
-        "script-src 'self' 'unsafe-inline' https://unpkg.com blob:; "
+        "script-src 'self' 'unsafe-inline' https://unpkg.com; "
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
         "img-src 'self' data:; "
         "font-src 'self' https://fonts.gstatic.com; "
